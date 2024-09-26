85ab3bb7f14f4212f3a36325749c2fdeb78af917
Matt Galindo <mgalindo15@pop-os.localdomain>
2024-09-26 18:46:26 -0500

    .

--- apps/chinese_racer/server.js
+++ apps/chinese_racer/server.js
@@ -1,6 +1,7 @@
 // server.js dependencies
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
@@ -11,6 +12,7 @@ const users = [];
 
 // Middleware for parsing request bodies
 app.use(bodyParser.json());
+app.use(cors());
 
 // Routes
 
